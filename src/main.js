6cd4a7c2a0fa51ee78b37001ef8f5440bb9dc916
nicgirault <nic.girault@gmail.com>
2016-09-16 20:18:13 +0200

    add cities with population scale

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { geoBaker } from 'd3-geo-projection';
-import { geoPath } from 'd3-geo';
+import { geoPath, geoMercator } from 'd3-geo';
 import { select } from 'd3-selection';
+import { scaleSqrt } from 'd3-scale';
+import { max } from 'd3-array';
 
 const populations = require('./populations')
 const world = require('./world.geojson')
@@ -9,7 +10,9 @@ require('./style.css');
 const bindTo = (divId) => {
   const width = 900, height = 600
 
-  const projection = geoBaker()
+  const projection = geoMercator()
+    .scale(170)
+    .translate([480,230])
 
   const path = geoPath().projection(projection)
 
@@ -20,7 +23,6 @@ const bindTo = (divId) => {
 
   const map = svg.append('g')
     .attr('class', 'map')
-  console.log(map)
 
   map.selectAll('.geo-path')
     .data(world.features)
@@ -31,6 +33,34 @@ const bindTo = (divId) => {
 		.attr("d", path)
 		.style("stroke-opacity", 1)
 		.style("fill-opacity", 0.5)
+
+  populations.forEach(function(population){
+    const position = projection([population.longitude, population.latitude])
+    population.x = position[0]
+    population.y = position[1]
+  })
+
+  const rScale = scaleSqrt()
+    .range([0, 10])
+    .domain([0, max(populations, function(d) { return d.population })])
+
+  const cities = svg.append('g')
+    .attr('class', 'cities')
+
+  cities.selectAll('city')
+    .data(populations)
+    .enter()
+    .append('circle')
+    .attr('class', 'city')
+    .attr('r', function(d) {
+      return rScale(d.population)
+    })
+    .attr('cx', function(d) {
+      return d.x
+    })
+    .attr('cy', function(d) {
+      return d.y
+    })
 }
 
 module.exports = bindTo;
